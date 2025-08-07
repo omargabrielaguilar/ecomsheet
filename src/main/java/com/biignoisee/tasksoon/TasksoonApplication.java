7d490b571ff91be658c0c3445ac86ed93f47bd47
omar <omareegab@gmail.com>
2025-08-07 18:33:08 -0500

    tasksoon
    tasksoon

---
+++ src/main/java/com/biignoisee/tasksoon/TasksoonApplication.java
@@ -0,0 +1,13 @@
+package com.biignoisee.tasksoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksoonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biignoisee/tasksoon/TasksoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biignoisee.tasksoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
