aa8bd5c623467fb66e25cc20008fc8bab8316a76
Omar <omareegab@gmail.com>
2025-08-27 15:02:41 -0500

    phpdoc

--- ecom/tests/Feature/DeleteAccountTest.php
+++ ecom/tests/Feature/DeleteAccountTest.php
@@ -3,16 +3,27 @@
 namespace Tests\Feature;
 
 use App\Models\User;
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use InvalidArgumentException;
 use Laravel\Jetstream\Features;
 use Laravel\Jetstream\Http\Livewire\DeleteUserForm;
 use Livewire\Livewire;
+use PHPUnit\Framework\Exception;
+use PHPUnit\Framework\ExpectationFailedException;
+use PHPUnit\Framework\SkippedWithMessageException;
 use Tests\TestCase;
 
 class DeleteAccountTest extends TestCase
 {
     use RefreshDatabase;
 
+    /**
+     * @throws BindingResolutionException
+     * @throws SkippedWithMessageException
+     * @throws InvalidArgumentException
+     * @throws ExpectationFailedException
+     */
     public function test_user_accounts_can_be_deleted(): void
     {
         if (! Features::hasAccountDeletionFeatures()) {
@@ -28,6 +39,13 @@ class DeleteAccountTest extends TestCase
         $this->assertNull($user->fresh());
     }
 
+    /**
+     * @throws BindingResolutionException
+     * @throws SkippedWithMessageException
+     * @throws ExpectationFailedException
+     * @throws Exception
+     * @throws InvalidArgumentException
+     */
     public function test_correct_password_must_be_provided_before_account_can_be_deleted(): void
     {
         if (! Features::hasAccountDeletionFeatures()) {
